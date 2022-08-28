1e4120de46b5ef7f0d10511a5d580871f7306415
Lindsey Heagy <lindseyheagy@gmail.com>
2022-08-27 17:33:48 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 # init mamba
 mamba init
+source ~/.bashrc
 
 # install environment for user
 mamba env create -f environment.yml --prefix ./envs
 
 # activate enviornment
-conda activate ./envs
+mamba activate ./envs
 python -m ipykernel install --user --name=e350
 
 # success
